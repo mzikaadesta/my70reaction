554d373555fe89a7341135a56dbcccc2b9026a82
mzikaadesta <m.zikaadesta@gmail.com>
2026-01-05 23:42:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.snow()
+import datetime
+import streamlit as st
+
+event_time = st.datetime_input(
+    "Schedule your event",
+    datetime.datetime(2025, 11, 19, 16, 45),
+)
+st.write("Event scheduled for", event_time)
